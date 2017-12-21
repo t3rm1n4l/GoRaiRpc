0201e7fd85d68244c1414dcf3d449b2000bdf196
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-12-21 14:10:14 +0100

    Rpc: bubble error to end user and don't execute a fatal inside of the lib

--- example/example.go
+++ example/example.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"fmt"
-	xrpc "github.com/devmahno/GoRaiRpc"
+	xrpc "github.com/donutloop/GoRaiRpc"
+	"log"
 )
 
 func main() {
-	rpc := xrpc.NewRaiRpc()
+	rpc := xrpc.New()
 	fmt.Printf("Begin\n")
 
-	fmt.Printf("AvailableSupply: %s\n", rpc.RpcAvailableSupply("raw"))
-	showData(rpc.RpcVersion())
+	availableSupply, err := rpc.RpcAvailableSupply("raw")
+
+	fmt.Printf("AvailableSupply: %s\n",availableSupply)
+
+	data, err := rpc.RpcVersion()
+	if err != nil {
+		log.Println(err)
+	}
+	showData(data)
 
 	fmt.Printf("-\n")
-	showData(rpc.RpcAccountInfo("xrb_3cwsajkzdycgg6k4q7zea68t4h5tu3ui59rdriurtjjem7rrjqtoixu473bt", "Mrai", true, true, true))
+	data, err = rpc.RpcAccountInfo("xrb_3cwsajkzdycgg6k4q7zea68t4h5tu3ui59rdriurtjjem7rrjqtoixu473bt", "Mrai", true, true, true)
+	if err != nil {
+		log.Println(err)
+	}
+	showData(data)
+
 
 	fmt.Printf("-RpcAccountsPending\n")
 	//accounts, count = '4096', threshold = 0, unit = 'raw', source = false
-	showData(rpc.RpcAccountsPending([]string{"xrb_1111111111111111111111111111111111111111111111111117353trpda",
-	"xrb_3t6k35gi95xu6tergt6p69ck76ogmitsa8mnijtpxm9fkcm736xtoncuohr3"}, "1", 1, "Mrai", true))
+	data, err = rpc.RpcAccountsPending([]string{"xrb_1111111111111111111111111111111111111111111111111117353trpda",
+	"xrb_3t6k35gi95xu6tergt6p69ck76ogmitsa8mnijtpxm9fkcm736xtoncuohr3"}, "1", 1, "Mrai", true)
+	if err != nil {
+		log.Println(err)
+	}
+	showData(data)
+
 
 	fmt.Printf("-RpcPending\n")
 	//account, count = '4096', threshold = 0, unit = 'raw', source = false
-	showData(rpc.RpcPending("xrb_1111111111111111111111111111111111111111111111111117353trpda", "1", 1, "Mrai", true))
+	data, err = rpc.RpcPending("xrb_1111111111111111111111111111111111111111111111111117353trpda", "1", 1, "Mrai", true)
+	if err != nil {
+		log.Println(err)
+	}
+	showData(data)
 
 	fmt.Printf("-RpcBlock\n")
 	fmt.Println(rpc.RpcBlock("3BDCF72E7662DB9E2F38B15AF8FD1B633232928ED323C44E83D63573889E9BAE"))
@@ -32,7 +54,11 @@ func main() {
 
 	fmt.Printf("-RpcBlocks\n")
 	hashes := []string{"3BDCF72E7662DB9E2F38B15AF8FD1B633232928ED323C44E83D63573889E9BAE"}
-	showData(rpc.RpcBlocks(hashes))
+	data, err = rpc.RpcBlocks(hashes)
+	if err != nil {
+		log.Println(err)
+	}
+	showData(data)
 
 	fmt.Printf("-RpcRepresentatives\n")
 	fmt.Println(rpc.RpcRepresentatives("Mrai", "1048576", "false"))
@@ -41,7 +67,12 @@ func main() {
 	fmt.Println(rpc.RpcUcheckedKeys("03C9B2BDEAD1F1982BAF16A8E66CFCD493B464533151BC01BA27963B0AAF6D81", "1"))
 
 	fmt.Printf("-RpcMraiToRaw\n")
-	fmt.Printf("MraiToRaw: %v\n", rpc.RpcMraiToRaw("1"))
+
+	mrai, err := rpc.RpcMraiToRaw("1")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("MraiToRaw: %v\n", mrai)
 
 	fmt.Printf("-ToUnit\n")
 	fmt.Printf("%v\n", rpc.ToUnit("1", "Mrai", "raw"))
